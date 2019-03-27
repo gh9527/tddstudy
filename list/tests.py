49add8b076c28aae1487768d987902559537c960
unknown <1040230003@qq.com>
2019-03-27 13:11:34 +0800

    bastic view now returns minimal HTML

--- list/tests.py
+++ list/tests.py
@@ -1,5 +1,8 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
+
 from list.views import home_page
 
 # Create your tests here.
@@ -7,3 +10,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')
         self.assertEqual(found.func,home_page)
+
+    def test_home_page_return_correct_html(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>To-Do list</title>',html)
+        self.assertTrue(html.endswith('</html>'))
