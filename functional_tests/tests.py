8418324450781cfd63bda4514b2de575681f47a4
unknown <1040230003@qq.com>
2019-03-28 14:51:33 +0800

    make functional_tests an app,use liveserverTestCase

---
+++ functional_tests/tests.py
@@ -0,0 +1,52 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+from django.test import LiveServerTestCase
+
+class NewVistorTest(LiveServerTestCase):
+
+    def setUp(self):
+        self.brower = webdriver.Chrome()
+
+    def tearDown(self):
+        self.brower.quit()
+
+    def check_for_row_in_list_table(self,row_text):
+        table = self.brower.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text,[row.text for row in rows])
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+                
+        #访问网站首页
+        self.brower.get(self.live_server_url)
+        #网站标题和头部包含'To-Do'这个词
+        self.assertIn('To-Do',self.brower.title)
+        header_text = self.brower.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do',header_text)
+        
+        #应用邀请她输入一个代办事项
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'),
+            'enter a to-do item'
+        )
+        #她在一个文本框中输入了’buy peacock feather‘
+        #她按回车键后，页面更新了
+        #代办事向表显示了’1：buy peacock feather‘
+        inputbox.send_keys('1：buy peacock feather')
+        inputbox.send_keys(Keys.ENTER)
+        time.sleep(3)
+        self.check_for_row_in_list_table('1：buy peacock feather')
+        #页面有显示了一个文本框，可以输入其他代办事项
+        #她输入了’use peacock feathers to make a fly
+        inputbox = self.brower.find_element_by_id('id_new_item')
+        inputbox.send_keys('2:use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1：buy peacock feather')
+        self.check_for_row_in_list_table('2:use peacock feathers to make a fly')
+        time.sleep(3)
+        self.fail('finish test')
+        #页面再次更新
+        #网站为她生成唯一url
+
